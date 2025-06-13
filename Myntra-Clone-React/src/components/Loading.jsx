10adacb1a02f9056bc66cc6a967a9f0d8bc1eb3c
Rishav Kumar <rishavkumar@Rishavs-MacBook-Air.local>
2025-06-13 12:22:34 +0530

    rspnsonsee

--- Myntra-Clone-React/src/components/Loading.jsx
+++
@@ -1,9 +0,0 @@
-const Loading = ()=>{
- return <center> 
- <button className="mt-5 btn btn-primary " type="button" disabled >
- <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>
- <span role="status">Loading...</span>
-</button>
-</center>
-}
-export default Loading
